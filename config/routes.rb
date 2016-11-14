61d6e618de9444e7f523a7bcb316f86d1f09ac82
amax3002 <alexandremguy@gmil.com>
2016-11-14 14:57:15 -0500

    finished basic

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome', controller: 'welcome', action: :index
-  get 'welcome/:name', controller: 'welcome', action: :special_message
-  get 'welcome/lorem/:lorium_type', controller: 'welcome', action: :lorium
+  get '/', controller: 'welcome', action: :index
+  get '/:name', controller: 'welcome', action: :special_message
+  get '/lorium/:lorium_type', controller: 'welcome', action: :lorium
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
