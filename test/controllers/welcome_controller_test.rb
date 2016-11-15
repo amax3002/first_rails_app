2b61a1b64a7f38be0c94d31ca0dc5fc8091b2faf
amax3002 <alexandremguy@gmil.com>
2016-11-15 09:37:12 -0500

    updating tests

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get welcome_url
+  test "should get welcome" do
+    get "/"
     assert_response :success
+  end
+
+  test "should get unique message" do
+    get "/Alex"
+    assert_response :success
+    assert_match(/Alex/, response.body)
+  end
+
+  test "should get lorium" do
+    get "/lorium/standard"
+    assert_response :success
+    assert_match(/consectetur adipiscing/, response.body)
   end
 end
